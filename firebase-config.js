2efa4b9517f1622611aa7dc7c2028e305fe03b8d
tettouqr <tettou.etsuran02@gmail.com>
2025-07-24 22:39:58 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,12 +2,12 @@
 // STEP1でコピーした、あなた自身のFirebase設定情報を貼り付けてください。
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDfskC92FG47SGSc_CPGG2s8DeOcZiRyPc",
-    authDomain: "fanclub-goods-system.firebaseapp.com",
-    projectId: "fanclub-goods-system",
-    storageBucket: "fanclub-goods-system.firebasestorage.app",
-    messagingSenderId: "738476621067",
-    appId: "1:738476621067:web:abd699999de9019f5a360e"
+    apiKey: "AIzaSyDekScDxMZCF8KBofcwWiBImHuwGBv_mFY",
+    authDomain: "qlover-system.firebaseapp.com",
+    projectId: "qlover-system",
+    storageBucket: "qlover-system.firebasestorage.app",
+    messagingSenderId: "767836761501",
+    appId: "1:767836761501:web:1b87bcd0374230eb869374"
   };
 
 // ------------------- ▲▲▲ この部分を書き換える ▲▲▲ -------------------
